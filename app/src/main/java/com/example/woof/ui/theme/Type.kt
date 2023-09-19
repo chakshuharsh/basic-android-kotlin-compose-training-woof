0ef534110e69823ec8a9134f79c018818ed065b7
Chakshu <chakshuharsh97@gmail.com>
2023-09-19 15:57:15 +0530

    Finally

--- app/src/main/java/com/example/woof/ui/theme/Type.kt
+++ app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -15,8 +15,11 @@
  */
 package com.example.woof.ui.theme
 
+
 import androidx.compose.material3.Typography
 
-// Set of Material typography styles to start with
+
+
+
 val Typography = Typography(
 )
